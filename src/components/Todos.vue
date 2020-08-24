93aa459ee06d98600466a2739b797b5cebaa750e
nishida <k.nishida@vistage-llc.com>
2020-08-24 11:02:53 +0900

    finish

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,8 +1,25 @@
 <template>
     <div>
         <h3>Todos</h3>
+        <div class="legend">
+            <span>Double click to mark as complete</span>
+            <span>
+                <span class="incomplete-box"></span>
+                = Incomplete
+            </span>
+            <span>
+                <span class="complete-box"></span>
+                = Complete
+            </span>
+        </div>
         <div class="todos">
-            <div v-for="todo in allTodos" :key="todo.id" class="todo">
+            <div
+                @dblclick="onDbClick(todo)"
+                v-for="todo in allTodos"
+                :key="todo.id"
+                class="todo"
+                v-bind:class="{ 'is-complete': todo.completed }"
+            >
                 {{ todo.title }}
                 <i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
             </div>
@@ -15,7 +32,18 @@ import { mapGetters, mapActions } from "vuex";
 
 export default {
     name: "Todos",
-    methods: { ...mapActions(["fetchTodos", "deleteTodo"]) },
+    methods: {
+        ...mapActions(["fetchTodos", "deleteTodo", "updateTodo"]),
+        onDbClick(todo) {
+            const updTodo = {
+                id: todo.id,
+                title: todo.title,
+                completed: !todo.completed,
+            };
+
+            this.updateTodo(updTodo);
+        },
+    },
     computed: mapGetters(["allTodos"]),
     created() {
         this.fetchTodos();
